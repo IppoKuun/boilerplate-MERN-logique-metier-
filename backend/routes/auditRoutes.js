ae052d2e68a1b03fc9f04442e8a673251dd79ee5
Hippolyte Diallo <ippokunn@gmail.com>
2025-10-20 20:21:04 +0200

    feat(auditRoutes) : auditRoute fini

--- backend/routes/auditRoutes.js
+++ backend/routes/auditRoutes.js
@@ -1,15 +1,25 @@
 import { Router } from "epxress";
 import AuditsEvents from "../models/AuditsEvents";
-import requireAuth from "../middlewares/requireAuth";
 import {pagination, buildMeta} from "../utils/pagination"
 import queryBuilder from "../utils/queryBuilder"
 
-const router = router()
+const auditRouter = Router()
 
-router.get("/", pagination({
-    defaultLimit : 50, maxLimit : 100
-}), queryBuilder({
-                allowedSort : new Set (["actor.user", "taget.slug"]),
+
+auditRouter.get("/",   async(req, res) => {
+ const { filter, sort, sortBy, order } =   queryBuilder(req.query, {
+            allowedSort : new Set (["actor.user", "taget.slug"]),
             equals : new Set (["actor.user", "target.slug"]),
             ranges : new Set (["ts"]),
-}))
+        }) 
+            const {page, limit, skip} =  pagination(req.query, { defaultLimit: 50, maxLimit: 200 });
+
+            const [items, total] = await Promise.all([
+            AuditsEvents.filter(filter).sort(sort).limit(limit).lean(),
+            AuditsEvents.countDocuments(filter)
+        ])
+          res.json({ items, meta: buildMeta({ page, limit, total, sortBy, order }) });
+    })
+
+
+module.exports = auditRouter
